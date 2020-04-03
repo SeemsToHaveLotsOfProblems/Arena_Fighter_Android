8ae2f44f8fb074d4e65e604f8e8a3d720d9faaa8
FatalErrorDriveB <bshipe21@outlook.com>
2020-04-03 09:52:07 -0400

    I don't know, I did all sorts of crap

--- app/src/main/java/com/fatalerrorapps/arena_fighter_android/MainActivity.java
+++ app/src/main/java/com/fatalerrorapps/arena_fighter_android/MainActivity.java
@@ -2,13 +2,39 @@ package com.fatalerrorapps.arena_fighter_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Creating the buttons
+    Button joinArenaButton = findViewById(R.id.joinArenaButton);
+    Button settingButton = findViewById(R.id.settingsButton);
+
+    View.OnClickListener handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(v == joinArenaButton){
+                //Send user to name input
+                Intent waitingRoomIntent = new Intent(MainActivity.this, WaitingRoom.class);
+                MainActivity.this.startActivity(waitingRoomIntent);
+
+            }//End joinArenaButton
+
+            if(v == settingButton){
+                //send user to settings menu
+
+            }//end settingsButton
+
+        }//End onClick
+    };//End On click listener
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }//End onCreate
+}//End Class
--- app/src/main/java/com/fatalerrorapps/arena_fighter_android/logo.java
+++ app/src/main/java/com/fatalerrorapps/arena_fighter_android/logo.java
@@ -2,15 +2,30 @@ package com.fatalerrorapps.arena_fighter_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class logo extends AppCompatActivity {
+
+    View.OnClickListener handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent mainActivityIntent = new Intent(logo.this, MainActivity.class);
+            logo.this.startActivity(mainActivityIntent);
+        }//EndOnClick
+    };//End OnClickListener
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
+
+        //Test code
+        Intent mainActivityIntent = new Intent(logo.this, MainActivity.class);
+        logo.this.startActivity(mainActivityIntent);
     }
 }
