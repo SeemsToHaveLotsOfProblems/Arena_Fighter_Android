83cf89d9d1b3941cf943798cd62645dd20fe6761
FatalErrorDriveB <bshipe21@outlook.com>
2020-04-03 15:56:32 -0400

    Linked a few things

--- app/src/main/java/com/fatalerrorapps/arena_fighter_android/NameEntry.java
+++ app/src/main/java/com/fatalerrorapps/arena_fighter_android/NameEntry.java
@@ -2,7 +2,11 @@ package com.fatalerrorapps.arena_fighter_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NameEntry extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class NameEntry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_entry);
-    }
-}
+
+        Button submitName = findViewById(R.id.submitNameButton);
+
+        submitName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Store name into variable and pass into WaitingRoom intent
+                EditText playerChosenName = findViewById(R.id.nameEntryEditText);
+                String playerName = playerChosenName.getText().toString();
+
+                Intent callWaitingRoom = new Intent(NameEntry.this, WaitingRoom.class);
+                if(playerName.isEmpty()){
+                    NameEntry.this.startActivity(callWaitingRoom);
+                } else {
+                    callWaitingRoom.putExtra(playerName, playerName);
+                    NameEntry.this.startActivity(callWaitingRoom);
+                }
+
+            }//End onClick
+        });//End submitName button
+
+    }//End onCreate
+}//End NameEntry
