a9bae0338f400a7731a947b18b53bcf9e26f53e2
FatalErrorDriveB <bshipe21@outlook.com>
2020-04-03 09:16:25 -0400

    Added animated logo splash screen

---
+++ app/src/main/java/com/fatalerrorapps/arena_fighter_android/logo.java
@@ -0,0 +1,16 @@
+package com.fatalerrorapps.arena_fighter_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+public class logo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
