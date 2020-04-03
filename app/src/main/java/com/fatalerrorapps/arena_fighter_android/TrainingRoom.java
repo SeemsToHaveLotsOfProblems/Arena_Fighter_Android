ab9a29266810c5cb0c4eebf9945c6ad76689fff1
FatalErrorDriveB <bshipe21@outlook.com>
2020-04-03 08:17:23 -0400

    Added waiting room & training room layout and class files.

---
+++ app/src/main/java/com/fatalerrorapps/arena_fighter_android/TrainingRoom.java
@@ -0,0 +1,14 @@
+package com.fatalerrorapps.arena_fighter_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrainingRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_room);
+    }
+}
