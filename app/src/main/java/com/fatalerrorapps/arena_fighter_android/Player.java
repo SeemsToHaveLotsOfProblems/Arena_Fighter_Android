b98a3157f28f07a20fee488dd0a91192e833e9a3
FatalErrorDriveB <bshipe21@outlook.com>
2020-04-04 08:32:44 -0400

    Started some programming

---
+++ app/src/main/java/com/fatalerrorapps/arena_fighter_android/Player.java
@@ -0,0 +1,47 @@
+package com.fatalerrorapps.arena_fighter_android;
+
+import java.util.Random;
+
+public class Player {
+
+    //Global Variables
+    String playerName = "Aaren";
+    static int playerCash = 15;
+    static int playerFans = 0;
+    static int playerStrength = 1;
+    static int playerEndurance = 5;
+    static int playerFatigue = 1;
+    static int totalFights = 0;
+    static int fightsWon = 0;
+    static int fightsLost = 0;
+
+    public static int statTraining(int statToTrain){
+        //1=strength, 2=endurance, 3=fatigue
+
+        int growth = 0;
+
+        Random rand = new Random();
+        int timesToTrain = 0;
+        if(playerFatigue > 1){
+            timesToTrain += rand.nextInt(playerFatigue);
+        } else {
+            timesToTrain += rand.nextInt(2);
+        }
+
+        for(int i = 0; i < timesToTrain; i++){
+            growth += rand.nextInt(5);
+        }//end for
+
+        if (statToTrain == 1){
+            playerStrength += growth;
+        } else if(statToTrain == 2) {
+            playerEndurance += growth;
+        } else {
+            playerFatigue += growth;
+        }
+
+        return growth;
+
+    }//End statToTrain
+
+}//End Player
--- app/src/main/java/com/fatalerrorapps/arena_fighter_android/WaitingRoom.java
+++ app/src/main/java/com/fatalerrorapps/arena_fighter_android/WaitingRoom.java
@@ -3,6 +3,7 @@ package com.fatalerrorapps.arena_fighter_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WaitingRoom extends AppCompatActivity {
 
@@ -10,5 +11,33 @@ public class WaitingRoom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting_room);
-    }
-}
+
+        Player player = new Player();
+
+        //Player Name
+        TextView playerName = findViewById(R.id.nameTextView);
+        playerName.setText(player.playerName);
+        //Player Cash
+        TextView playerCash = findViewById(R.id.cashTextView);
+        playerCash.setText(Player.playerCash);
+        //Player Strength
+        TextView playerStrength = findViewById(R.id.strengthTextView);
+        playerStrength.setText(Player.playerStrength);
+        //Player Endurance
+        TextView playerEndurance = findViewById(R.id.enduranceTextView);
+        playerEndurance.setText(Player.playerEndurance);
+        //Player Fatigue
+        TextView playerFatigue = findViewById(R.id.fatigueTextview);
+        playerFatigue.setText(Player.playerFatigue);
+        //Total Fights
+        TextView totalFights = findViewById(R.id.totalFightsTextView);
+        totalFights.setText(Player.totalFights);
+        //Fights Won
+        TextView fightsWon = findViewById(R.id.fightsWonTextView);
+        fightsWon.setText(Player.fightsWon);
+        //Fights Lost
+        TextView fightsLost = findViewById(R.id.fightsLostTextView);
+        fightsLost.setText(Player.fightsLost);
+
+    }//End onCreate
+}//End WaitingRoom
